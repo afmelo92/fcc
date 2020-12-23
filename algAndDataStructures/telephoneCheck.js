02e9b36d5c75fc6c9be5758c61c1cf1c4b239f84
Andre <andre.fabian.melo@gmail.com>
2020-12-22 23:12:15 -0300

    cash register

--- algAndDataStructures/telephoneCheck.js
+++ algAndDataStructures/telephoneCheck.js
@@ -1,27 +1,26 @@
-// function telephoneCheck(str) {
-//   var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
-//   return regex.test(str);
-// }
-
 function telephoneCheck(str) {
   var re = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
   return re.test(str);
 }
 
+// function telephoneCheck(str) {
+//   var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+//   return regex.test(str);
+// }
+console.log('::::::::::::::::::TRUE:::::::::::::::::::::::::')
 console.log(telephoneCheck("555-555-5555"))
 console.log(telephoneCheck("(555)555-5555"))
 console.log(telephoneCheck("(555) 555-5555"))
 console.log(telephoneCheck("5555555555"))
 console.log(telephoneCheck("1 555 555 5555"))
-console.log(':::::::::::::::::::::::::::::::::::::::::::')
 
 
+console.log('::::::::::::::::::FALSE:::::::::::::::::::::::::')
 console.log(telephoneCheck("1 555)555-5555"))
 console.log(telephoneCheck("(6054756961)"))
 console.log(telephoneCheck("2 (757) 622-7382"))
 console.log(telephoneCheck("0 (757) 622-7382"))
 console.log(telephoneCheck("-1 (757) 622-7382"))
-
 console.log(telephoneCheck("2 757 622-7382"))
 console.log(telephoneCheck("10 (757) 622-7382"))
 console.log(telephoneCheck("27576227382"))
